c59f8f34c095d357447ec706d268fa8d924940f2
marekgacek45 <marekgacek45@gmail.com>
2024-12-03 13:00:23 +0100

    create all routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Livewire\Livewire;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\OtherPagesController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
@@ -12,4 +18,16 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['lo
     });
 
     Route::get('/', HomeController::class)->name('home');
+    Route::get('/o-nas', AboutController::class)->name('about');
+    Route::get('/pokoje', [RoomController::class,'index'])->name('room.index');
+    Route::get('/pokoje/{slug}', [RoomController::class,'index'])->name('room.show');
+    Route::get('/lokalizacja', LocationController::class)->name('location');
+    Route::get('/galeria', GalleryController::class)->name('gallery');
+    Route::get('/kontakt', ContactController::class)->name('contact');
+    Route::get('/polityka-prywatnosci', [OtherPagesController::class,'privacy-policy'])->name('privacy-policy');
+
+
+    Route::fallback(function () {
+        return redirect('/');
+    });
 });
