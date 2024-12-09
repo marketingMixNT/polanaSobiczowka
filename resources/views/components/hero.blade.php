e4e14b5dbc9e07615e6a693e9d9fba3a5e567e0f
marekgacek45 <marekgacek45@gmail.com>
2024-12-09 10:52:39 +0100

    update content on about page

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,20 +1,26 @@
-@props(['image','title','subtitle'])
+@props(['image','imageMobile','title','subtitle'])
 
-<section
-    class="w-[99%] h-[80vh] bg-cover bg-center bg-fixed mx-auto bg-blend-multiply bg-gray-400 flex flex-col justify-center items-center relative"
-    style="background-image:url({{$image}})">
+<section class="w-[99%] h-[80vh] mx-auto flex flex-col justify-center items-center relative">
 
-<div class="font-heading  text-fontLight  tracking-wide z-50 text-center flex flex-col justify-center items-center gap-16">
+    <div class="absolute inset-0 bg-black opacity-30 z-0"></div>
 
-    <h1 class=" px-6 md:px-12 text-center text-4xl xs:text-5xl lg:text-7xl       font-extralight"
-    style="line-height: 1.2">
-   {{$title}}
-</h1>
-<span class=" text-sm lg:text-2xl font-thin font-text ">
-    {{$subtitle}}</span>
+    <img src="{{$imageMobile}}" alt="wnętrze apartamentu w Apartamenty Polana Sobiczkowa w Kościelisku"
+        class="sm:hidden w-full h-full object-cover object-center absolute top-0 left-0 -z-10" />
 
-</div>
+    <img src="{{$image}}" alt="wnętrze apartamentu w Apartamenty Polana Sobiczkowa w Kościelisku"
+        class="hidden sm:block w-full h-full object-cover object-center absolute top-0 left-0 -z-10" />
 
-<x-link-btn class="absolute bottom-32 flex justify-center items-center w-[200px] " href="">Zarezerwuj</x-link-btn>
+    <div
+        class="font-heading text-fontLight tracking-wide z-20 text-center flex flex-col justify-center items-center gap-16">
+        <h1 class="px-6 md:px-12 text-center text-4xl xs:text-5xl lg:text-7xl font-extralight" style="line-height: 1.2">
+            {{$title}}
+        </h1>
+        <span class="text-sm lg:text-2xl font-thin font-text">
+            {{$subtitle}}
+        </span>
+    </div>
 
+    <x-link-btn class="absolute bottom-32 flex justify-center items-center w-[200px] z-30" href="{{route('contact')}}">
+        Zarezerwuj
+    </x-link-btn>
 </section>
--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -1,18 +1,19 @@
-<x-layouts.app title="about" description="meta">
+<x-layouts.app title="O nas - Polana Sobiczkowa | Komfortowe Apartamenty w Górach"
+    description="Polana Sobiczkowa to komfortowe apartamenty w góralskim stylu, idealne na wypoczynek. Zaledwie 3 km od Zakopanego, z bliskością atrakcji Podhala.">
 
     <x-layouts.app-wrapper>
 
         {{-- hero --}}
-        <x-hero image="{{asset('assets/hero.jpg')}}" title="O nas" subtitle="Poznaj nas" />
+        <x-hero image="{{asset('assets/images/desktop/polana-sobiczkowa-32.webp')}}"
+            imageMobile="{{asset('assets/images/mobile/polana-sobiczkowa-32.webp')}}" title="O nas"
+            subtitle="Poznaj nas" />
 
         {{-- text --}}
         <x-text-section>
-            Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Repellendus quia nihil quibusdam odit veritatis sapiente impedit
-            placeat minima facere, at alias dolorum deserunt! Ex pariatur beatae ad cum nemo officia rerum
-            aliquam. Similique aliquid error eos, adipisci quis facere voluptate maxime id harum doloribus alias
-            quo dolores voluptates omnis rerum expedita sequi officiis! Aut quidem voluptates sapiente
-            laudantium magni neque.
+            <strong>Polana Sobiczkowa</strong> to miejsce, które łączy tradycję z nowoczesnością. Nasze komfortowe
+            apartamenty w góralskim stylu zapewniają wygodę i przestronność, idealne dla rodzin oraz grup przyjaciół.
+            <br><br>Zaledwie 3 km od Zakopanego, oferujemy ciszę i spokój, jednocześnie będąc w pobliżu atrakcji
+            Podhala. To doskonała baza wypadowa na górskie wędrówki, narciarstwo i odpoczynek w otoczeniu natury.
         </x-text-section>
 
         {{-- block --}}
@@ -25,20 +26,27 @@
                     <x-text-image-block-text>
 
                         <x-subheading-block>
-                            Lorem ipsum dolor sit amet consectetur.
+                            Przestrzeń dla każdego
                         </x-subheading-block>
 
                         <x-heading-block large>
-                            Lorem ipsum dolor sit amet consectetur.
+                            Odpoczynek w sercu Podhala
                         </x-heading-block>
 
                         <x-text-block large>
-                            Lorem ipsum dolor sit, amet
-                            consectetur adipisicing elit. Perferendis temporibus non architecto dicta asperiores
-                            corrupti
-                            pariatur commodi eos sit, repudiandae ipsa, atque voluptatibus porro reprehenderit. Numquam,
-                            aliquam ipsum molestias necessitatibus vel cum, quas nihil quasi suscipit, fugiat molestiae
-                            repellendus cumque?
+                            Polana Sobiczkowa to miejsce, które powstało z pasji do gór oraz gościnności. Chcieliśmy
+                            stworzyć przestrzeń, która łączy w sobie tradycyjny góralski klimat z nowoczesnymi
+                            udogodnieniami. Nasze apartamenty zapewniają wygodę, przestronność oraz unikalny klimat,
+                            który sprzyja zarówno relaksowi, jak i aktywnemu wypoczynkowi. Każdy detal został
+                            zaprojektowany z myślą o naszych gościach, by poczuli się jak w domu, odpoczywając w
+                            otoczeniu pięknej przyrody.
+                            <br><br>
+                            Nasze apartamenty to nie tylko komfortowe wnętrza, ale także idealna lokalizacja, zaledwie 3
+                            km od Zakopanego. Jesteśmy doskonałą bazą wypadową do górskich wędrówek, narciarstwa czy
+                            innych atrakcji Podhala. Wierzymy, że w Polana Sobiczkowa każdy znajdzie coś dla siebie,
+                            niezależnie od tego, czy szuka spokoju w otoczeniu natury, czy aktywnego wypoczynku w
+                            górach.
+
                         </x-text-block>
 
                     </x-text-image-block-text>
@@ -46,7 +54,13 @@
                     {{-- image --}}
                     <div class="min-h-[400px]">
                         <div class=" h-full w-full">
-                            <img src="{{asset('assets/hero.jpg')}}" alt="" class="w-full h-full object-cover">
+                            <img src="{{asset('assets/images/mobile/polana-sobiczkowa-40.webp')}}"
+                            alt="wnętrze apartamentu w Apartamenty Polana Sobiczkowa w Kościelisku"
+                            class="sm:hidden w-full h-full object-cover">
+                            
+                        <img src="{{asset('assets/images/desktop/polana-sobiczkowa-40.webp')}}"
+                            alt="wnętrze apartamentu w Apartamenty Polana Sobiczkowa w Kościelisku"
+                            class="hidden sm:block w-full h-full object-cover">
                         </div>
 
                     </div>
@@ -64,28 +78,40 @@
                     {{-- image --}}
                     <div class="min-h-[400px]">
                         <div class=" h-full w-full">
-                            <img src="{{asset('assets/hero.jpg')}}" alt="" class="w-full h-full object-cover">
+                            <img src="{{asset('assets/images/mobile/polana-sobiczkowa-43.webp')}}"
+                                alt="wnętrze apartamentu w Apartamenty Polana Sobiczkowa w Kościelisku"
+                                class="sm:hidden w-full h-full object-cover">
+                                
+                            <img src="{{asset('assets/images/desktop/polana-sobiczkowa-43.webp')}}"
+                                alt="wnętrze apartamentu w Apartamenty Polana Sobiczkowa w Kościelisku"
+                                class="hidden sm:block w-full h-full object-cover">
                         </div>
 
                     </div>
                     {{-- text --}}
-                    <x-text-image-block-text >
+                    <x-text-image-block-text>
 
                         <x-subheading-block>
-                            Lorem ipsum dolor sit amet consectetur.
+                            Twoje miejsce w sercu gór
                         </x-subheading-block>
 
                         <x-heading-block large>
-                            Lorem ipsum dolor sit amet consectetur.
+                            Góry, komfort i wyjątkowa atmosfera
                         </x-heading-block>
 
                         <x-text-block large>
-                            Lorem ipsum dolor sit, amet
-                            consectetur adipisicing elit. Perferendis temporibus non architecto dicta asperiores
-                            corrupti
-                            pariatur commodi eos sit, repudiandae ipsa, atque voluptatibus porro reprehenderit. Numquam,
-                            aliquam ipsum molestias necessitatibus vel cum, quas nihil quasi suscipit, fugiat molestiae
-                            repellendus cumque?
+                            Polana Sobiczkowa to miejsce, które zostało stworzone z myślą o tych, którzy szukają
+                            spokoju, komfortu i niezapomnianych widoków. Nasze apartamenty, urządzone w tradycyjnym
+                            góralskim stylu, zapewniają pełną wygodę i przestronność, którą docenią zarówno rodziny, jak
+                            i grupy przyjaciół. Każdy szczegół naszej oferty jest zaprojektowany z myślą o Twoim
+                            komforcie, a naszym celem jest stworzenie atmosfery, w której poczujesz się jak u siebie w
+                            domu.
+                            <br><br>
+                            Chcieliśmy połączyć tradycję z nowoczesnością, aby nasi goście mogli cieszyć się spokojem
+                            górskiego klimatu, nie rezygnując z wygody. Polana Sobiczkowa to nie tylko miejsce na
+                            nocleg, ale także przestrzeń do odpoczynku, relaksu i tworzenia wspomnień. Po dniu pełnym
+                            wrażeń możesz wrócić do komfortowego apartamentu, w którym poczujesz się zrelaksowany i
+                            wypoczęty.
                         </x-text-block>
 
                     </x-text-image-block-text>
